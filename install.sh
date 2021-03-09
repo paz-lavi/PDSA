a98e46c7756104d7debbc9b131ac813ad789f6f2
paz-lavi <pazlavi29@gmail.com>
2021-03-09 18:56:15 +0200

    uninstall

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 git clone https://github.com/paz-lavi/PDSA.git
 cd PDSA
-chmod +x pdsa
+chmod +x pdsa uninstall
 make
 mv validate-key-and-file.o /usr/local/bin
 mv sign-file.o /usr/local/bin
 mv validate-signed-file.o /usr/local/bin
 mv keys-generator.o /usr/local/bin
 mv pdsa /usr/local/bin
+mv uninstall /usr/local/bin
 cd ..
 rm -rf PDSA
-
